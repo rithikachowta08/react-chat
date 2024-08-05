f576505b11e6eb8111f8e9e8feeb6759112adacd
rithikachowta08 <rithikachowta08@gmail.com>
2024-08-05 16:02:33 +0530

    Fix errors

--- src/lib/rootSaga.js
+++ src/lib/rootSaga.js
@@ -1,9 +1,11 @@
-import { all, takeEvery } from 'redux-saga/effects';
-import ACTIONS from './actions.constants';
-import { updateChatState, fetchChat } from './Chat.saga';
+import { all, takeEvery } from "redux-saga/effects";
+import ACTIONS from "./actions.constants";
+import { updateChatState, fetchChat } from "./Chat.saga";
 
-export default () =>
+const rootSaga = () =>
   all([
     takeEvery(ACTIONS.FETCH_CHAT, fetchChat),
-    takeEvery(ACTIONS.UPDATE_CHAT, updateChatState)
+    takeEvery(ACTIONS.UPDATE_CHAT, updateChatState),
   ]);
+
+export default rootSaga;
